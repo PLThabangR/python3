d35d68282ecd5f3f0edb8e578004f35d2c696d18
PLThabangR <rakgoropthabang@gmail.com>
2024-02-23 03:22:21 +0200

    type casting

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+print("Hello world")
+#User input
+num1 = input("Enter a number?")
+num2 = input("Enter a number?")
+
+num11=int(num1)
+num12=int(num1)
+print(type(num12))
+print(num11 + num12)
---
+++ variables.py
@@ -0,0 +1,24 @@
+#CamelCase
+# x=myName
+#SnakeCase
+# y= my_name
+
+a,b,c =1,2,3
+print(a,b,c)
+
+x = ("Thabang",30,"June")
+print(type(x))
+print(x[2])
+
+my_name= "Tha" \
+"ba" \
+"ng"
+print(my_name)
+length=len(my_name)
+print("the lengt of my string is",length)
+
+mul="this " \
+"is a"\
+"Long String"
+print(mul)
+
