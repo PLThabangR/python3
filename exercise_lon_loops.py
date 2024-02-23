a134a39cf8529cd1ec53f0e2abe1dd44c327826b
PLThabangR <rakgoropthabang@gmail.com>
2024-02-23 05:05:39 +0200

    Nested loops program

---
+++ exercise_lon_loops.py
@@ -0,0 +1,10 @@
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+count = 0
+for x,num in enumerate(num_list):
+    count += 1
+    if num == 36:
+        print('Number found at ', x)
+        break
+
+print(count)
